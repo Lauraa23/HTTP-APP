8f77fdac4900b078568e4cdb076d6dab073d8d0d
Laura Pino <lpigueroa@gmail.com>
2024-01-24 16:08:48 +0100

    Cambios

---
+++ src/breakingbad/breakingbad-app.js
@@ -0,0 +1,32 @@
+const fetchQuote = async () => {
+  const res = await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
+  const data = await res.json();
+
+  console.log(data[0]);
+  return data[0];
+};
+
+export const BreakingbadApp = async (element) => {
+  document.querySelector("#app-title").innerHTML = "Breaking Bad App";
+  element.innerHTML = "Loading...";
+
+  //await fetchQuote();
+  const quoteLabel = document.createElement("blockquote");
+  const authorLabel = document.createElement("h3");
+  const nextQuoteButton = document.createElement("button");
+  nextQuoteButton.innerText = "Next Quote";
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.quote;
+    authorLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+  };
+
+  nextQuoteButton.addEventListener("click", async () => {
+    element.innerHTML = "Loading...";
+    const quote = await fetchQuote();
+    renderQuote(quote);
+  });
+
+  fetchQuote().then(renderQuote);
+};
